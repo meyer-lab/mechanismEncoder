8106234c02e0637981aeaacffc92d43b0c0d564e
Sara JC Gosline <sara.gosline@pnnl.gov>
2020-12-08 15:21:36 -0800

    added two more cancer types
    this should make the dataset complete as far as the `cptac` api allows

--- phosphoData/phosphoPDCdata.py
+++ phosphoData/phosphoPDCdata.py
@@ -16,7 +16,7 @@ def main():
     opts = parser.parse_args()
 
     if opts.get:
-        for ds in ['brca', 'ccrcc', 'colon', 'ovarian']:
+        for ds in ['brca', 'ccrcc', 'colon', 'ovarian','endometrial','luad']:
             cptac.download(dataset=ds)
 
     if opts.type.lower() == 'brca':
@@ -27,13 +27,19 @@ def main():
         dat = cptac.Colon()
     elif opts.type.lower() == 'ovca':
         dat = cptac.Ovarian()
+    elif opts.type.lower() == 'luad':
+        dat = cptac.Luad()
+    elif opts.type.lower() == 'endometrial':
+        dat = cptac.Endometrial()
     else:
         exit()
 
     df = dat.get_phosphoproteomics()
+    pdf = dat.get_proteomics()
    # df.columns = [' '.join(col).strip() for col in df.columns.values]
 
-    df.to_csv(path_or_buf="file.tsv",sep='\t')
-
+    df.to_csv(path_or_buf="phos_file.tsv",sep='\t')
+    pdf.to_csv(path_or_buf='prot_file.tsv',sep='\t')
+    
 if  __name__=='__main__':
     main()
