82c1ee55aed728b1e6dde4936805fe20c95261a2
FFroehlich <fabian@schaluck.com>
2021-03-18 12:59:17 -0400

    model updates

--- pathways/pw_EGFR_MAPK.py
+++ pathways/pw_EGFR_MAPK.py
@@ -2,10 +2,10 @@ from pysb import Model, Observable
 
 from mEncoder.mechanistic_model import (
     add_monomer_synth_deg, generate_pathway, add_activation,
-    add_observables, add_inhibitor, add_gf_bolus
+    add_observables, add_inhibitor, add_gf_bolus, cleanup_unused
 )
 
-model = Model('EGFR_MAPK_AKT_STAT')
+model = Model('EGFR_MAPK')
 
 rtkfs = ['EGF']
 
@@ -17,29 +17,17 @@ for rtkf_name in rtkfs:
 
 erbb_cascade = [
     ('EGFR',  {'Y1173': ['EGF']}),
-    ('ERBB2', {'Y1248': ['EGF']}),
+    ('ERBB2', {'Y1248': ['EGFR__Y1173_p']}),
 ]
 generate_pathway(model, erbb_cascade)
 
 active_rtks = ['EGFR__Y1173_p', 'ERBB2__Y1248_p']
-stat_rtks = ['EGFR__Y1173_p', 'ERBB2__Y1248_p']
-
-# MAPK
-for ras_name in ['HRAS', 'KRAS', 'NRAS']:
-    add_monomer_synth_deg(ras_name, nsites=['N'])
-    add_activation(
-        model, ras_name, 'N', 'nucleotide_exchange',
-        active_rtks
-    )
 
 mapk_cascade = [
-    ('RAF1',  {'S338':      ['KRAS__N_gtp', 'HRAS__N_gtp', 'NRAS__N_gtp']}),
-    ('BRAF',  {'S445':      ['KRAS__N_gtp', 'HRAS__N_gtp', 'NRAS__N_gtp']}),
-    ('ARAF',  {'S299':      ['KRAS__N_gtp', 'HRAS__N_gtp', 'NRAS__N_gtp']}),
-    ('MAP2K1',   {'S218_S222': ['RAF1__S338_p', 'BRAF__S445_p',
-                                'ARAF__S299_p']}),
-    ('MAP2K2',   {'S222_S226': ['RAF1__S338_p', 'BRAF__S445_p',
-                                'ARAF__S299_p']}),
+    ('RAF1',  {'S338':      active_rtks + ['EGF']}),
+    ('BRAF',  {'S445':      active_rtks + ['EGF']}),
+    ('MAP2K1',   {'S218_S222': ['RAF1__S338_p', 'BRAF__S445_p']}),
+    ('MAP2K2',   {'S222_S226': ['RAF1__S338_p', 'BRAF__S445_p']}),
     ('MAPK1',  {'T185_Y187': ['MAP2K1__S218_p__S222_p',
                               'MAP2K2__S222_p__S226_p']}),
     ('MAPK3',  {'T202_Y204': ['MAP2K1__S218_p__S222_p',
@@ -62,3 +50,4 @@ add_inhibitor(model, 'iMEK', ['MAP2K1', 'MAP2K2'])
 #add_inhibitor(model, 'iPKC', ['MAP2K1', 'MAP2K2'])
 
 add_observables(model)
+
